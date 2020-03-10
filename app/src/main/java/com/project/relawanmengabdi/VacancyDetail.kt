9fe26efd715674e379f9398b47a1119b46276975
MilzamHutomo <milzamhutomo.social@gmail.com>
2020-03-10 20:47:51 +0700

    Finalize home and description layout

---
+++ app/src/main/java/com/project/relawanmengabdi/VacancyDetail.kt
@@ -0,0 +1,12 @@
+package com.project.relawanmengabdi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VacancyDetail : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vacancy_detail)
+    }
+}
